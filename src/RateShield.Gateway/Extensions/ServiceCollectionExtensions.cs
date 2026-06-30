386077a47f9d04fc8d59a365f5a30efbd0243667
nosaemma21 <emmanuelnosakhare052@gmail.com>
2026-06-30 06:53:37 +0100

    feat(config): binded and validated rateshield opts

---
+++ src/RateShield.Gateway/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+using RateShield.Core.Configuration;
+
+namespace RateShield.Gateway.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    // extension to include and validate the rateshield config opts
+    public static IServiceCollection AddRateShieldOptions(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services
+            .AddOptions<RateShieldOptions>()
+            .Bind(configuration.GetSection(RateShieldOptions.SectionName))
+            .Validate(
+                options => options.Policies.Count > 0,
+                "At least one rate limit policy must be configured."
+            )
+            .Validate(
+                options => options.Policies.ContainsKey("Default"),
+                "A Default rate limit policy must be configured"
+            //will remove default policy from json config
+            )
+            .Validate(
+                options => options.CleanUp.IntervalSeconds > 0,
+                "Cleanup options must be greater than zero"
+            )
+            .Validate(
+                options => options.CleanUp.BucketIdleTimeoutSeconds > 0,
+                "Bucket idle timeout must be greater than zero"
+            )
+            .Validate(
+                options => options.RejectionResponse.ContentType.Length > 0,
+                "Rejection response content type is required."
+            )
+            .Validate(
+                options => options.RejectionResponse.ErrorCode.Length > 0,
+                "Rejection response error code is required"
+            )
+            .Validate(
+                options => options.RejectionResponse.Message.Length > 0,
+                "Rejection response message is required"
+            )
+            .ValidateOnStart(); // this will fail on start up if configs don't meet my
+
+        return services;
+    }
+}
--- src/RateShield.Gateway/Program.cs
+++ src/RateShield.Gateway/Program.cs
@@ -1,6 +1,12 @@
+using RateShield.Gateway.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//addeing the rateshield options
+builder.Services.AddRateShieldOptions(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "RateShield gateway is running.");
 
 app.Run();
