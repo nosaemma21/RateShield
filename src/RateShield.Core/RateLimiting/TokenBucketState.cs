d50cb6648ad00f32cff3970f324772918651e27d
nosaemma21 <emmanuelnosakhare052@gmail.com>
2026-07-01 06:34:18 +0100

    docs(core): documented token bucket state

--- src/RateShield.Core/RateLimiting/TokenBucketState.cs
+++ src/RateShield.Core/RateLimiting/TokenBucketState.cs
@@ -16,7 +16,21 @@ public sealed class TokenBucketState
         LastSeenAt = lastSeenAt;
     }
 
+    /// <summary>
+    /// The number of tokens currently available for this bucket.
+    /// Requests can be allowed only when this value is greater than or equal to the request cost.
+    /// </summary>
     public int AvailableTokens { get; set; }
+
+    /// <summary>
+    /// The timestamp of the last completed refill calculation.
+    /// This is used to determine how many refill periods have elapsed since the bucket was updated.
+    /// </summary>
     public DateTimeOffset LastRefilledAt { get; set; }
+
+    /// <summary>
+    /// The timestamp of the most recent request seen for this bucket.
+    /// Cleanup workers use this value to remove buckets that have been idle for too long.
+    /// </summary>
     public DateTimeOffset LastSeenAt { get; set; }
 }
